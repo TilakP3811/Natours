7a0a23de9039698cf9faa66e26a67e3d025b71d8
Tilak <tilak@webkorps.com>
2022-11-08 02:13:41 +0530

    structuring the file and code better

---
+++ routes/tourRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const tourControllers = require("../controllers/tourControllers.js");
+
+router.get("/", tourControllers.getAllTours);
+router.post("/", tourControllers.createTour);
+router.get("/:id", tourControllers.getTour);
+router.patch("/:id", tourControllers.updateTour);
+router.delete("/:id", tourControllers.deleteTour);
+
+module.exports = router;
