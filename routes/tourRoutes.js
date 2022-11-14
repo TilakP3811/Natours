d5125abe434682c51a1518138c64f48787e48418
Tilak <tilak@webkorps.com>
2022-11-15 00:17:59 +0530

    adding bussiness logics with mongodb aggregrate pipline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const tourControllers = require('../controllers/tourControllers.js');
 
 router.get('/', tourControllers.getAllTours);
+router.get('/tour-stats', tourControllers.getTourStats);
+router.get('/monthly-plan/:year', tourControllers.getMonthlyPlan);
 router.post('/', tourControllers.createTour);
 router.get('/:id', tourControllers.getTour);
 router.patch('/:id', tourControllers.updateTour);
