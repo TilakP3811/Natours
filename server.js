730f5fae70e922075b46be4e935f45c2e6a2a5e3
Tilak <tilak@webkorps.com>
2022-11-17 01:04:27 +0530

    added unhandle rejection and uncaught exception outside the express

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app.js');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLE REJECTION! 💥 shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './env/config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -20,6 +26,14 @@ mongoose
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLE REJECTION! 💥 shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
