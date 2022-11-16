490fc4d85a84a52ab2c08cf52f2367da0f0c52da
Tilak <tilak@webkorps.com>
2022-11-17 00:26:15 +0530

    mongo db production best error message showing

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/appError');
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -20,6 +22,23 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+const handleCastError = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleMongoDuplicateField = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleMongoValidationError = (err) => {
+  const errors = Object.values(err.errors).map((ele) => ele.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -27,6 +46,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastError(error);
+    if (error.code === 11000) error = handleMongoDuplicateField(error);
+    if (err.name === 'ValidationError')
+      error = handleMongoValidationError(error);
+    sendErrorProd(error, res);
   }
 };
