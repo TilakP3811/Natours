eb8f6d386fcc91f4f1f7e6fc45e711f3ed570a70
Tilak <tilak@webkorps.com>
2022-12-10 00:20:43 +0530

    jwt token verification

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,18 @@
 const User = require('../models/userModel.js');
 const catchAsync = require('../utils/catchAsync.js');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({ name, email, password, passwordConfirm });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
